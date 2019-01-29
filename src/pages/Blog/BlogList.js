255132fdfc720d49bed1c4dccf20160e78b900f9
hezhipeng <yearhe@outlook.com>
2019-01-29 10:57:19 +0800

    123

--- src/pages/Blog/BlogList.js
+++ src/pages/Blog/BlogList.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Card, Table, Modal, Button, Tag, Icon } from 'antd';
+import { Card, Table, Modal, Button, Tag, Icon, Row, Col, Input, DatePicker, Form, Select } from 'antd';
 import { connect } from 'dva';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import { Link } from 'dva/router';
-import moment from 'moment'; 
+import moment from 'moment';
 import 'moment/locale/zh-cn'
 
-moment.locale('zh-cn');  
+moment.locale('zh-cn');
 
 const { confirm } = Modal;
+const { RangePicker } = DatePicker;
+const {Option} = Select;
 
 @connect(({ blogmodel }) => ({
     blogmodel,
@@ -42,7 +44,7 @@ class BlogList extends React.Component {
         title: '是否有效',
         dataIndex: 'enabled',
         key: 'enabled',
-        render: (text) => text ? <Icon type="smile" theme="twoTone" twoToneColor="#eb2f96" /> : <Icon type="frown" />
+        render: (text) => text ? <span><Icon type="smile" theme="twoTone" twoToneColor="#eb2f96" />有效</span> : <span><Icon type="frown" />无效</span>
     }, {
         title: '操作',
         key: 'action',
@@ -149,7 +151,40 @@ class BlogList extends React.Component {
         return (
           <PageHeaderWrapper title="博客列表">
             <Card>
-              <Button type="primary" shape="circle" icon="reload" loading={blogmodel.loading} onClick={this.btnClick} />
+              <Row gutter={16}>
+                <Col className="gutter-row" span={6}>
+                  <Form.Item label="标题" style={{ display: 'flex' }}><Input placeholder="default size" style={{ minWidth: 330 }} /></Form.Item>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                  <Form.Item label="时间" style={{ display: 'flex' }}><RangePicker style={{ minWidth: 330 }} /></Form.Item>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                  <Form.Item label="标签" style={{ display: 'flex' }}>
+                    <Select defaultValue="lucy" style={{ minWidth: 330 }}>
+                      <Option value="jack">Jack</Option>
+                      <Option value="lucy">Lucy</Option>
+                      <Option value="disabled">Disabled</Option>
+                      <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                  <Form.Item label="有效" style={{ display: 'flex' }}>
+                    <Select defaultValue="lucy" style={{ minWidth: 330 }}>
+                      <Option value="jack">Jack</Option>
+                      <Option value="lucy">Lucy</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col className="gutter-row" span={18}>
+                  <Button type="primary" shape="circle" icon="reload" loading={blogmodel.loading} onClick={this.btnClick} style={{ marginBottom: 5 }} />
+                </Col>
+                <Col className="gutter-row" span={6}>
+                  <Button type="primary" icon="search" ghost loading={blogmodel.loading} onClick={this.btnClick} style={{ marginBottom: 5 }}>搜索</Button>
+                </Col>
+              </Row>
               {/* <Button shape="circle" icon="plus" style={{ marginLeft: 5 }} onClick={this.add} /> */}
               <Table columns={this.columns} dataSource={blogmodel.dataSource} loading={blogmodel.loading} pagination={pagination} style={{ marginTop: 5 }} />
             </Card>
